6d27ca7a1d20b25d1ba9837ef194698c81508ae0
Ben Wu <wubin28@gmail.com>
2017-10-11 22:32:02 +0800

    write 2 acceptance criteria

---
+++ src/test/java/com/wubinben/oobootcamp/ParkingLotTest.java
@@ -0,0 +1,6 @@
+package com.wubinben.oobootcamp;
+
+public class ParkingLotTest {
+    // The parking lot can have the driver park his car if there is parking space available
+    // The parking lot can have the driver pick up his car if he returns the parking ticket
+}
