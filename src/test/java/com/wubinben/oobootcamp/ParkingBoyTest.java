56458769e726ce03b0b5ccfeeaceb474b817626b
Ben Wu <wubin28@gmail.com>
2017-10-12 00:32:42 +0800

    add testing notes for parking boy

---
+++ src/test/java/com/wubinben/oobootcamp/ParkingBoyTest.java
@@ -0,0 +1,8 @@
+package com.wubinben.oobootcamp;
+
+public class ParkingBoyTest {
+    // The parking boy can park the car in the single parking lot
+    // The parking boy can park the car in the second parking lot if the first one is full
+    // The parking boy can pick up the car from the single parking lot
+    // The parking boy can pick up the car from the second parking lot
+}
