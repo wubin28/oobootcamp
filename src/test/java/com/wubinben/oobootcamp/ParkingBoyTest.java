d68c56b8ecfbd87dd8f1e145e4a6e2a7298dedd6
Ben Wu <wubin28@gmail.com>
2017-10-12 00:54:05 +0800

    finish test the_parking_boy_can_park_the_car_in_the_second_parking_lot_if_the_first_one_is_full

--- src/test/java/com/wubinben/oobootcamp/ParkingBoyTest.java
+++ src/test/java/com/wubinben/oobootcamp/ParkingBoyTest.java
@@ -18,7 +18,24 @@ public class ParkingBoyTest {
 
         assertTrue(parkingBoy.isParkable());
     }
-    // The parking boy can park the car in the second parking lot if the first one is full
+
+    @Test
+    public void the_parking_boy_can_park_the_car_in_the_second_parking_lot_if_the_first_one_is_full() {
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot = new ParkingLot(10);
+        parkingLots.add(parkingLot);
+        ParkingLot anotherParkingLot = new ParkingLot(10);
+        parkingLots.add(anotherParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+
+        for (int i = 0; i < 10; i++) {
+            parkingLot.park("JA" + i);
+        }
+
+        assertTrue(parkingBoy.isParkable());
+    }
+
+    // The parking boy cannot park the car if all the parking lots are full
     // The parking boy can pick up the car from the single parking lot
     // The parking boy can pick up the car from the second parking lot
 }
