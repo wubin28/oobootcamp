5b1bc7d966dd685ac18741104b749718791129f4
Ben Wu <wubin28@gmail.com>
2017-10-12 00:56:55 +0800

    finish test the_parking_boy_cannot_park_the_car_if_all_the_parking_lots_are_full

--- src/test/java/com/wubinben/oobootcamp/ParkingBoyTest.java
+++ src/test/java/com/wubinben/oobootcamp/ParkingBoyTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class ParkingBoyTest {
 
@@ -35,7 +36,25 @@ public class ParkingBoyTest {
         assertTrue(parkingBoy.isParkable());
     }
 
-    // The parking boy cannot park the car if all the parking lots are full
+    @Test
+    public void the_parking_boy_cannot_park_the_car_if_all_the_parking_lots_are_full() {
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        ParkingLot parkingLot = new ParkingLot(10);
+        parkingLots.add(parkingLot);
+        ParkingLot anotherParkingLot = new ParkingLot(10);
+        parkingLots.add(anotherParkingLot);
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLots);
+
+        for (int i = 0; i < 10; i++) {
+            parkingLot.park("JA" + i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            anotherParkingLot.park("JB" + i);
+        }
+
+        assertFalse(parkingBoy.isParkable());
+    }
     // The parking boy can pick up the car from the single parking lot
     // The parking boy can pick up the car from the second parking lot
 }
